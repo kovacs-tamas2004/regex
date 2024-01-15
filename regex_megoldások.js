bbc58ec1cfbfc6fefe0c6f772d99a2dcd6a052dd
Kovács Tamás <144217056+kovacs-tamas2004@users.noreply.github.com>
2024-01-15 09:18:38 +0100

    Add files via upload

---
+++ regex_megoldások.js
@@ -0,0 +1,17 @@
+// 1. feladat:
+'hackerrank'
+
+// 2. feladat:
+/^(\S{3}\.)(\S{3}\.){2}(\S{3})$/g
+
+// 3. feladat:
+/\d{2}\D{1}\d{2}\D{1}\d{4}/
+
+// 4. feladat:
+/\S{2}\s\S{2}\s\S{2}/
+
+// 5. feladat:
+/\w{3}\W\w{10}\W\w{3}/
+
+// 6. feladat:
+/^\d\w{4}\.$/
